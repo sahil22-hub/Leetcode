7ea904a7a310be2ea01d789072d2311e4fc80066
Sahil Babu Bhetwal <61641446+sahil22-hub@users.noreply.github.com>
2025-02-01 16:29:16 -0600

    Time: 3 ms (43.44%) | Memory: 17.8 MB (30.69%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def isHappy(self, n: int) -> bool:   
         count = len(str(n))
-        track = set()
+        track = []
         while n != 1 and n not in track:
-            track.add(n)
+            track.append(n)
             n = sum(int(num)**2 for num in str(n))
         return n == 1
             
