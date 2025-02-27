f4ae328d0d4bbe50d8e093c7dc0de28872abea39
Sahil Babu Bhetwal <61641446+sahil22-hub@users.noreply.github.com>
2025-02-26 19:51:43 -0600

    Time: 3 ms (81.88%) | Memory: 18.7 MB (33.55%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        num_map = {}  # Dictionary to store numbers and their indices
+    
+        for i, num in enumerate(numbers):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement]+1, i+1] 
+            num_map[num] = i  
