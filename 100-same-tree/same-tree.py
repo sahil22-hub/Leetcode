05aa49321e0d2638482c2bcb46045bdfc5d16514
Sahil Babu Bhetwal <61641446+sahil22-hub@users.noreply.github.com>
2025-03-01 18:57:31 -0600

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.20%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -3,15 +3,32 @@
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
-#         self.right = right
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        if not p and q or p and not q:
-            return False
-        if p.val != q.val:
-            return False
+# #         self.right = right
+# class Solution:
+#     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+#         if not p and not q:
+#             return True
+#         if not p and q or p and not q:
+#             return False
+#         if p.val != q.val:
+#             return False
         
-        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right, q.right)
+#         return self.isSameTree(p.left,q.left) and self.isSameTree(p.right, q.right)
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        queue = deque([(p, q)])
+
+        while queue:
+            node1, node2 = queue.popleft()
+
+            if not node1 and not node2:
+                continue
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+
+            queue.append((node1.left, node2.left))
+            queue.append((node1.right, node2.right))
+
+        return True
         
