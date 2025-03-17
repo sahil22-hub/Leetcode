9781d6a6e8dbb0b982b970f75cc58f2929e018cc
Sahil Babu Bhetwal <61641446+sahil22-hub@users.noreply.github.com>
2025-03-16 23:50:49 -0500

    Time: 15 ms (51.27%) | Memory: 21.1 MB (48.45%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,37 +1,37 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dd = {}
-        for word in strs:
-            temp = "".join(sorted(word))
-            print(word,temp)
-            if temp in dd.keys():
-                dd[temp].append(word)
-            else:
-                dd[temp] = [word]
-        return list(dd.values())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # sorted_lst = ["".join(sorted(word)) for word in strs]
-        # print(sorted_lst)
-        # l = len(strs)
-        # temp = {}
-        # for i in range(l):
-        #     if sorted_lst[i] in temp.keys():
-        #         temp[sorted_lst[i]].append(strs[i])
+        # dd = {}
+        # for word in strs:
+        #     temp = "".join(sorted(word))
+        #     print(word,temp)
+        #     if temp in dd.keys():
+        #         dd[temp].append(word)
         #     else:
-        #         temp[sorted_lst[i]] = [strs[i]]
-        # print(temp.values())
-        # return list(temp.values())
+        #         dd[temp] = [word]
+        # return list(dd.values())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        sorted_lst = ["".join(sorted(word)) for word in strs]
+        print(sorted_lst)
+        l = len(strs)
+        temp = {}
+        for i in range(l):
+            if sorted_lst[i] in temp.keys():
+                temp[sorted_lst[i]].append(strs[i])
+            else:
+                temp[sorted_lst[i]] = [strs[i]]
+        print(temp.values())
+        return list(temp.values())
         
