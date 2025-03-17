fa3ece7520edd878cfe94e5ba46e8d4172123373
Sahil Babu Bhetwal <61641446+sahil22-hub@users.noreply.github.com>
2025-03-17 00:48:27 -0500

    Time: 46 ms (60.91%) | Memory: 18.1 MB (95.17%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,18 +1,51 @@
+from collections import defaultdict
 class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-        l = len(p)
-        sorted_ = "".join(sorted(p))
-        print(sorted_)
-        ll = len(s)
-        count = 0
-        output = []
-        while count <= ll-l:
-            temp = "".join(sorted(s[count:count+l]))
-            if sorted_ == temp:
-                output.append(count)
-            count += 1
-        return output
+    def findAnagrams(self,s: str, p: str) -> list[int]:
+        if len(p) > len(s):
+            return []
+
+        p_count = defaultdict(int)
+        s_count = defaultdict(int)
+
+        for char in p:
+            p_count[char] += 1
+
+        for i in range(len(p)):
+            s_count[s[i]] += 1
+
+        print(p_count)
+        print(s_count)
+        result = []
+
+        if s_count == p_count:
+            result.append(0)
+
+        for i in range(0,len(s)-len(p)):
+            left_char = s[i]
+            s_count[left_char] -= 1
+            if s_count[left_char] == 0:
+                del s_count[left_char]
+            right_char = s[i+len(p)]
+            s_count[right_char] += 1
+
+            if s_count == p_count:
+                result.append(i+1)
 
 
-        
-        
+        # for i in range(len(p), len(s)):
+        #     left_char = s[i - len(p)]
+        #     s_count[left_char] -= 1
+        #     if s_count[left_char] == 0:
+        #         del s_count[left_char]
+
+        #     right_char = s[i]
+        #     s_count[right_char] += 1
+
+        #     if s_count == p_count:
+        #         result.append(i - len(p) + 1)
+
+        return result
+
+
+            
+            
